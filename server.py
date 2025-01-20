055fa3693d4170763c7baa4d71de7f5d88cf4190
William Chang <none>
2025-01-20 11:24:27 -0800

    Add env handler

--- server.py
+++ server.py
@@ -1,8 +1,17 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def root_handler():
-    return "<p>hello</p>"
+  return "<a href='/env'>env</a>"
 
+@app.route("/env")
+def env_handler():
+  lines = []
+  for key, value in os.environ.items():
+    lines.append(f"{key}={value}")
+  output = "<p>%s</p>" % "<br>".join(lines)
+  return output
